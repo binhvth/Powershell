e9f6c37b95eaa4bc589255bb8bc2c1c4449f6288
binhvth <binhvth@outlook.com>
2025-04-29 09:48:41 +0700

    Create DellOldFiles.ps1
    Xóa files cũ, tự động xóa các file backup sql quá 3 ngày

---
+++ del_old_files/DellOldFiles.ps1
@@ -0,0 +1,14 @@
+﻿# Cấu hình
+$folderPath = "E:\backupwins\SQL"
+$daysOld = 3
+
+# Tính ngày giới hạn
+$limitDate = (Get-Date).AddDays(-$daysOld)
+
+# Tìm và xóa các file cũ
+Get-ChildItem -Path $folderPath -Recurse -File | Where-Object {
+    $_.LastWriteTime -lt $limitDate
+} | ForEach-Object {
+    Write-Host "Xóa: $($_.FullName)"
+    Remove-Item $_.FullName -Force
+}
